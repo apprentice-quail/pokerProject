ae6a45672df75aa6613bab044de3a0f8429dce39
ashlei <ashlei.jones@detroitlabs.com>
2017-04-03 11:44:14 -0400

    added basic notes

---
+++ pokerApp.js
@@ -0,0 +1,10 @@
+//2-7 players
+//straight flush (same suit) e.g. 5,6,7,8,9
+//four of a kind
+//full house (3 of a kind + 2 of a kind)
+//flush e.g. 5 cards of the same suit
+//straight (suit insensitive)
+//3 of a kind
+//2 pair
+//1 pair
+//high card

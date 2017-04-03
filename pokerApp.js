89b60c54003c6a59118d119693dc4cbced774715
ashlei <ashlei.jones@detroitlabs.com>
2017-04-03 12:50:57 -0400

    adding functions to validate player amount

--- pokerApp.js
+++ pokerApp.js
@@ -10,3 +10,33 @@
 //high card
 
 var sget = require('sget');
+maxPlayersAllowed = 7;
+
+var validatePlayerAmount = function(isThisManyPlayersOK) {
+	if(isThisManyPlayersOK > maxPlayersAllowed) {
+		console.log("Sorry, only " + maxPlayersAllowed + " people may play at a time.");
+		startPrompt();
+	} else if (isThisManyPlayersOK < 2) {
+		console.log("You'll need at least 2 players.");
+		startPrompt();
+	}  else if (isThisManyPlayersOK) {
+		return isThisManyPlayersOK;
+	}
+};
+
+var startPrompt = function() {
+	var howManyPlayers = sget("How many people are playing?").trim();
+		if (!howManyPlayers.match(/^[0-9]+$/)) {
+				console.log("Please use a number.");
+				startPrompt();
+		} else {
+			numberOfPlayers = parseInt(howManyPlayers);
+			validatePlayerAmount(numberOfPlayers);
+
+			if (numberOfPlayers >= 2 && numberOfPlayers <= 7) {
+				console.log(numberOfPlayers + " players are playing.");
+			}
+		}
+};
+
+startPrompt();
